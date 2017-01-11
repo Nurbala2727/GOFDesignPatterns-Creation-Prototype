bb404e8be6ae061d0b30581baf8a65cc19e8dcc8
Adam Reid <adameastcoast@gmail.com>
2017-01-11 00:33:42 -0500

    Add a dog to the cloning!

--- src/main/java/com/training/prototype/Client.java
+++ src/main/java/com/training/prototype/Client.java
@@ -10,5 +10,11 @@ public class Client {
 
         System.out.println("Dolly hash: " + dolly.hashCode());
         System.out.println("Cloned hash: " + clonedDolly.hashCode());
+
+        Dog doggy = new Dog();
+        Dog snoopy = (Dog)cloneAnimals.getClone(doggy);
+
+        System.out.println("Dog hash: " + doggy.hashCode());
+        System.out.println("Snoopy hash: " + snoopy.hashCode());
     }
 }
