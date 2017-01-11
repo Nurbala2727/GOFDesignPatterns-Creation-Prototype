b1152f204f0f1c969845da487f343004dc27b835
Adam Reid <adameastcoast@gmail.com>
2017-01-11 00:28:42 -0500

    Add client logic.

--- src/main/java/com/training/prototype/Client.java
+++ src/main/java/com/training/prototype/Client.java
@@ -2,6 +2,13 @@ package com.training.prototype;
 
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Dummy main!");
+        CloneFactory cloneAnimals = new CloneFactory();
+
+        Sheep dolly = new Sheep();
+
+        Sheep clonedDolly = (Sheep)cloneAnimals.getClone(dolly);
+
+        System.out.println("Dolly hash: " + dolly.hashCode());
+        System.out.println("Cloned hash: " + clonedDolly.hashCode());
     }
 }
