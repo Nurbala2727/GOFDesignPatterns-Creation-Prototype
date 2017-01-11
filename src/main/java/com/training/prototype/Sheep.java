af34b2b282eec5418ce6658777bdd3a175a5d584
Adam Reid <adameastcoast@gmail.com>
2017-01-11 00:22:42 -0500

    Add Sheep class and methods.

--- src/main/java/com/training/prototype/Sheep.java
+++ src/main/java/com/training/prototype/Sheep.java
@@ -2,4 +2,27 @@ package com.training.prototype;
 
 public class Sheep implements Animal {
 
+    @Override
+    public Animal makeClone() {
+        Sheep sheep = new Sheep();
+
+        try {
+            sheep = (Sheep) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            //todo use logger.
+        }
+
+        return sheep;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
