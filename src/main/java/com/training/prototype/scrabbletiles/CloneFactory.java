8f40326ee79b95cf19e00857bda21ab9f50d70d5
Adam Reid <adameastcoast@gmail.com>
2017-01-11 00:49:15 -0500

    Add clone factory

--- src/main/java/com/training/prototype/scrabbletiles/CloneFactory.java
+++ src/main/java/com/training/prototype/scrabbletiles/CloneFactory.java
@@ -3,5 +3,8 @@ package com.training.prototype.scrabbletiles;
 /**
  * Created by adame on 1/11/2017.
  */
-public class CloneFactory {
+public class CloneFactory{
+    public Tile cloneTile(Tile tile) {
+        return tile.makeClone();
+    }
 }
